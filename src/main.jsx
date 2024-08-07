a338d2d6af05ced277357419cce26e2a25f35709
Данил <danikkach666@gmail.com>
2024-08-07 21:11:56 +0300

    add create main.jsx and index.css

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import 'react-datepicker/dist/react-datepicker.css';
+import './index.css';
+import 'modern-normalize';
+import App from './App.jsx';
+import { store, persistor } from './redux/store.js';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
